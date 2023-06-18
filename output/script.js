4984a0d5df4117724099b1cb15645f066ce3b1a6
arifur rahman - office <arifur.rahman@smartdatasoft.com>
2023-06-18 16:00:17 +0600

    example 1

--- output/script.js
+++ output/script.js
@@ -1,3 +1,27 @@
 "use strict";
-const country = "Bangladesh I love you!";
-console.log(country);
+/* let playerName;
+console.log(playerName);
+
+playerName =  "Musfiqur";
+
+playerName =  34;
+
+
+console.log(playerName); */
+/* function multiply(a: number, b: number) {
+    return a * b;
+}
+
+console.log(multiply(4, 4)); */
+/* let mixed = ['apple', 3, true, 'orange', {}];
+
+mixed.push({
+    name: "Sakib"
+});
+ */
+let person = {
+    name: 'Musfiq',
+    age: 35,
+    isCaptain: true
+};
+person.country = "Bangladesh";
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,33 @@
-const country  ="Bangladesh I love you!";
+/* let playerName;
+console.log(playerName);
 
-console.log(country);
+playerName =  "Musfiqur";
+
+playerName =  34;
+
+
+console.log(playerName); */
+
+
+/* function multiply(a: number, b: number) {
+    return a * b;
+}
+
+console.log(multiply(4, 4)); */
+
+
+/* let mixed = ['apple', 3, true, 'orange', {}];
+
+mixed.push({
+    name: "Sakib"
+});
+ */
+
+
+let person = {
+    name: 'Musfiq',
+    age: 35,
+    isCaptain: true
+}
+
+person.country = "Bangladesh";
