2d18cbe7cf828674469b15b4c27b21c4f1972849
arifur rahman - office <arifur.rahman@smartdatasoft.com>
2023-06-19 17:21:59 +0600

    lesson 11 done

--- output/script.js
+++ output/script.js
@@ -1,18 +1,8 @@
-"use strict";
-class Player {
-    constructor(n, a, c) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
-    }
-    play() {
-        console.log(`${this.name} from ${this.country} is playing!`);
-    }
-}
 const musfiq = new Player('Musfiq', 40, 'Bangladesh');
 const sakib = new Player('Sakib', 38, 'Bangladesh');
-sakib.name = "Musfiq";
-console.log(sakib.name);
+console.log(sakib.age);
+console.log(sakib.country);
 const players = [];
 players.push(musfiq);
 players.push(sakib);
+export {};
--- src/script.ts
+++ src/script.ts
@@ -1,36 +1,8 @@
-class Player {
-    /*  private name: string;
-     public age: number;
-     readonly country: string;
-  */
-    /*     constructor(n: string, a: number, c: string) {
-    
-            this.name = n;
-            this.age = a;
-            this.country = c;
-        } */
-
-    constructor(
-        private name: string,
-        public age: number,
-        readonly country: string
-
-    ) { }
-
-    play() {
-        console.log(`${this.name} from ${this.country} is playing!`);
-    }
-}
-
+import { Player } from "./classes/Player.js";
 
 const musfiq = new Player('Musfiq', 40, 'Bangladesh');
 const sakib = new Player('Sakib', 38, 'Bangladesh');
 
-sakib.name = "Musfiq";
-sakib.age = 40;
-sakib.country = 'England';
-
-console.log(sakib.name);
 console.log(sakib.age);
 console.log(sakib.country);
 
